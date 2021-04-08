cfe241dd89c3c79397a61684ef739fad6aec0015
harurunrunrun <68173582+harurunrunrun@users.noreply.github.com>
2021-04-08 22:18:24 +0900

    Add files via upload
    open(0) only

---
+++ library/Input2.py
@@ -0,0 +1,49 @@
+class INPUT:
+  def __init__(self):
+    self.l=open(0).read().split()[::-1]
+    self.length=len(self.l)
+    return
+
+  def stream(self,k=1,f=int,f2=False):
+    assert(-1<k)
+    m=self.length
+    if m==0 or m<k:
+      raise Exception("There is no input!")
+    elif f!=str:
+      if k==0:
+        self.length=0
+        return list(map(f,self.l[::-1]))
+      if k==1 and not f2:
+        self.length-=1
+        return f(self.l.pop())
+      if k==1 and f2:
+        self.length-=1
+        return [f(self.l.pop())]
+      ret=[]
+      for _ in [0]*k:
+        ret.append(f(self.l.pop()))
+      self.length-=k
+      return ret
+    else:
+      if k==0:
+        self.length=0
+        return self.l[::-1]
+      if k==1 and not f2:
+        self.length-=1
+        return self.l.pop()
+      if k==1 and f2:
+        self.length-=1
+        return [self.l.pop()]
+      ret=[]
+      for _ in [0]*k:
+        ret.append(self.l.pop())
+      self.length-=k
+      return ret
+pin=INPUT().stream
+
+"""
+pin(number[default:1],f[default:int],f2[default:False])
+if number==0 -> return left all
+eclipse 等では_input.txtを使うこと。
+listを変数で受け取るとき、必ずlistをTrueにすること。
+"""
