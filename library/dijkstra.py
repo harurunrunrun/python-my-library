eaffaec7d499166fc2b78056de9dc7309b42f025
harurunrunrun <68173582+harurunrunrun@users.noreply.github.com>
2021-04-13 09:10:44 +0900

    Update dijkstra.py

--- library/dijkstra.py
+++ library/dijkstra.py
@@ -1,8 +1,8 @@
 class dijkstra:
-  def __init__(self):
+  def __init__(self,V):
     self.INF=float("inf")
-    self.G=[[] for _ in [0]*100000]
-    self.d=[self.INF]*100000
+    self.G=[[] for _ in [0]*V]
+    self.V=V
     return
   
   def add1(self,s,t,v):
@@ -16,20 +16,21 @@ class dijkstra:
 
   def run(self,s):
     from heapq import heapify,heappop,heappush
+    ret=[self.INF]*self.V
     que=[]
     heapify(que)
-    self.d[s]=0
+    ret[s]=0
     heappush(que,[0,s])
     while que:
       p=heappop(que)
       v=p[1]
-      if self.d[v]<p[0]:
+      if ret[v]<p[0]:
         continue
       for i in range(len(self.G[v])):
         e=self.G[v][i]   
-        if self.d[e[0]]>self.d[v]+e[1]:
-          self.d[e[0]]=self.d[v]+e[1]
-          heappush(que,[self.d[e[0]],e[0]])
-    return self.d
+        if ret[e[0]]>ret[v]+e[1]:
+          ret[e[0]]=ret[v]+e[1]
+          heappush(que,[ret[e[0]],e[0]])
+    return ret
 
 
