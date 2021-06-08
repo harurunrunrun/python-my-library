dd263f967249550fc2fb4a1a9c4acc44c8a64ada
harurunrunrun <68173582+harurunrunrun@users.noreply.github.com>
2021-06-09 00:26:33 +0900

    Create projection.py

---
+++ library/projection.py
@@ -0,0 +1,11 @@
+def projection(xp1,yp1,xp2,yp2,xp,yp):
+  assert((xp1,yp1)!=(xp2,yp2))
+  if xp1==xp2:
+    return (xp1,yp)
+  if yp1==yp2:
+    return (xp,yp1)
+  xm=xp1-xp2
+  ym=yp1-yp2
+  x=((xm**2)*xp+(ym**2)*xp1-xm*ym*(yp1-yp))/(xm**2+ym**2)
+  y=((xm**2)*ym*yp1-xm*(ym**2)*(xp1-xp)+(ym**3)*yp)/((xm**2)*ym+ym**3)
+  return (x,y)
